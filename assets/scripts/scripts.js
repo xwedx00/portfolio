d4ad6a7ae9f320486b78e091eb45aaa1e337c653
xwedx00 <xwedx00@protonmail.com>
2023-09-30 13:23:48 +0500

    --- Auto Git Commit ---

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,26 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let typeSplit = new SplitType('[animate]', {
-      types: 'lines, words, chars',
-      tagName: 'h1'
-    })
+let typeSplit = new SplitType('[animate]', {
+    types: 'lines, words, chars',
+    tagName: 'h1'
+  })
   
-    gsap.from('[animate] .char', {
-      opacity: 0,
-      duration: 1,
-      ease: 'power1.out',
-      stagger: 0.1,
-    })
-  
-    let typeSplit = new SplitType('[animate]', {
-      types: 'lines, words, chars',
-      tagName: 'span'
-    })
-  
-    gsap.from('[animate] .char', {
-      opacity: 0,
-      duration: 1,
-      ease: 'power1.out',
-      stagger: 0.1,
-    })
-  });
-  
+  gsap.from('[animate] .char', {
+    opacity: 0,
+    duration: 1,
+    ease: 'power1.out',
+    stagger: 0.1,
+  })
