7c5f51ebf30449a03f9771287d3f7f00a2ff7e58
xwedx00 <xwedx00@protonmail.com>
2023-09-30 13:09:47 +0500

    --- Auto Git Commit ---

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,7 +1,25 @@
 gsap.from("#page1 h1",{
-    y:30,
-    opacity:0,
-    delay:0.5,
-    duration:0.9,
-    stagger:0.2
+    <script src="https://unpkg.com/split-type"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script>
+
+<script>
+let typeSplit = new SplitType('[animate]', {
+  types: 'lines, words, chars',
+  tagName: 'span'
+})
+
+gsap.from('[animate] .word', {
+  opacity: 0.3,
+  duration: 0.5,
+  ease: 'power1.out',
+  stagger: 0.1,
+  
+  scrollTrigger: {
+    trigger: '[animate]',
+    start: 'top center',
+    scrub: true
+  }
+})
+</script>
 })
