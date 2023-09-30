7e499e4b0e3e853938e1c1e574108d3e73c2a4e3
xwedx00 <xwedx00@protonmail.com>
2023-09-30 13:11:32 +0500

    --- Auto Git Commit ---

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,22 +1,7 @@
-
-
-
-
-
-let typeSplit = new SplitType('[animate]', {
-  types: 'lines, words, chars',
-  tagName: 'h1'
-})
-
-gsap.from('[animate] .word', {
-  opacity: 0.3,
-  duration: 0.5,
-  ease: 'power1.out',
-  stagger: 0.1,
-  
-  scrollTrigger: {
-    trigger: '[animate]',
-    start: 'top center',
-    scrub: true
-  }
+gsap.from("#page1 h1",{
+    y:30,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    stagger:0.2
 })
