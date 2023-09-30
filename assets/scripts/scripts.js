110d75de93bddc6f0f88de19f5f59fe0fdefca4e
xwedx00 <xwedx00@protonmail.com>
2023-09-30 13:27:03 +0500

    --- Auto Git Commit ---

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,25 +1,11 @@
 let typeSplit = new SplitType('h1', {
     types: 'lines, words, chars',
     tagName: 'h1'
-  })
-  
-  gsap.from('h1 .char', {
-    opacity: 0,
-    duration: 1,
-    ease: 'power1.out',
-    stagger: 0.1,
-  })
-  <script src="https://unpkg.com/split-type"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
-
-<script>
-let typeSplit = new SplitType('[animate]', {
-  types: 'lines, words, chars',
-  tagName: 'span'
 })
 
-gsap.from('[animate] .char', {
-  
-  
+gsap.from('h1 .char', {
+    opacity: 0.3,
+    duration: 3,
+    ease: 'elastic.inOut',
+    stagger: 0.25,
 })
-</script>
