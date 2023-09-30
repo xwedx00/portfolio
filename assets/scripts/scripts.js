bb13b47d0eb9516f346f9b3d2e78c4e929f62069
xwedx00 <xwedx00@protonmail.com>
2023-09-30 13:13:32 +0500

    --- Auto Git Commit ---

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,8 +6,8 @@ gsap.from("#page1 h1",{
     stagger:0.2
 })
 
-<script src="https://unpkg.com/split-type"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
+
+
 
 <script>
 let typeSplit = new SplitType('[animate]', {
