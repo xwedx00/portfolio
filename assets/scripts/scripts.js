708df7015fbcadb98fb929ec9e8cfe5f57aa9663
xwedx00 <xwedx00@protonmail.com>
2023-09-30 13:26:33 +0500

    --- Auto Git Commit ---

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -9,4 +9,17 @@ let typeSplit = new SplitType('h1', {
     ease: 'power1.out',
     stagger: 0.1,
   })
+  <script src="https://unpkg.com/split-type"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
+
+<script>
+let typeSplit = new SplitType('[animate]', {
+  types: 'lines, words, chars',
+  tagName: 'span'
+})
+
+gsap.from('[animate] .char', {
   
+  
+})
+</script>
