2f76bdfbe22516eef323df0e25f09ef013bc8bfc
xwedx00 <xwedx00@protonmail.com>
2023-09-30 13:16:17 +0500

    --- Auto Git Commit ---

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,13 +1,3 @@
-gsap.from("#page1 h1",{
-    y:30,
-    opacity:0,
-    delay:0.5,
-    duration:0.9,
-    stagger:0.2
-})
-
-
-
 let typeSplit = new SplitType('[animate]', {
   types: 'lines, words, chars',
   tagName: 'span'
