7ba22ede7affc62808adc3612203ae383dfdaaed
xwedx00 <xwedx00@protonmail.com>
2023-09-30 13:10:17 +0500

    --- Auto Git Commit ---

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -10,7 +10,7 @@ gsap.from("#page1 h1",{
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script>
 
-<script>
+
 let typeSplit = new SplitType('[animate]', {
   types: 'lines, words, chars',
   tagName: 'span'
